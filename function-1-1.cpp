dfd5122cfe8bd117ffd814fe55c4852ef3df7fc5
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-08-24 17:00:49 +0930

    first commit

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int *left_shift(int *vals, int len, int amount){
-    
+    for(int i = 0; i < len;i++){
+        if(amount + i >= len){
+            *(vals + i) = 0;
+            
+        }else {
+            
+        *(vals + i) = *(vals + amount + i);
+        }
+
+    }
+    return vals;
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,5 +3,9 @@
 extern  int *left_shift(int *vals, int len, int amount);
 
 int main(int argc, char **argv){
-
+    int vals[]={1,2,3,4,5};
+    int *sv=left_shift(vals,5,2);
+    
+    for(int i = 0; i < 5;i++){
+        std :: cout << *(sv + i) << " ";
 }
